e943f2eb9d06a4f78761c2a8ba29eb1623c1218d
Th1nkK1D <witheep@gmail.com>
2021-01-14 16:41:29 +0700

    Update tailwind extractor to support svelte class binding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const { tailwindExtractor } = require('tailwindcss/lib/lib/purgeUnusedStyles');
 
 module.exports = {
   purge: {
     mode: 'all',
     content: ['./src/**/*.svelte', './src/**/*.html'],
+    options: {
+      defaultExtractor: (content) => [
+        ...tailwindExtractor(content),
+        // Match Svelte class: directives (https://github.com/tailwindlabs/tailwindcss/discussions/1731)
+        ...[...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(
+          ([_match, group, ..._rest]) => group
+        ),
+      ],
+      keyframes: true,
+    },
   },
   darkMode: false,
   theme: {
