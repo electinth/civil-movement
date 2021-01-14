ea1574f9a005fbd4aede8b2c84e10644ed90c49a
Th1nkK1D <witheep@gmail.com>
2021-01-14 12:03:02 +0700

    Add fontsize and color in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,28 @@ module.exports = {
     content: ['./src/**/*.svelte'],
     enabled: production,
   },
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
+    fontSize: {
+      36: 36,
+      24: 24,
+      21: 21,
+      18: 18,
+      17: 17,
+      16: 16,
+      15: 15,
+      14: 14,
+      12: 12,
+      10: 10,
+    },
+    colors: {
+      white: 'white',
+      gray: '#A9A9A9',
+      black: 'black',
+      mint: '#88FFCC',
+      'blue-light': '#0A98FF',
+      'blue-dark': '#0000CD',
+    },
     extend: {},
   },
   variants: {
