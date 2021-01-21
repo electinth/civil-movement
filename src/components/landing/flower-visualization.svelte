c283056339975ee350c1204a0dd430c97f13b1e3
Th1nkK1D <witheep@gmail.com>
2021-01-21 18:27:01 +0700

    Map tooltip to node position

--- src/components/landing/flower-visualization.svelte
+++ src/components/landing/flower-visualization.svelte
@@ -5,23 +5,35 @@
 
   import movements from '../../assets/data/event_all.csv';
 
-  interface NodeData {
+  interface Node {
     name: string;
     date: Date;
+    x: number;
+    y: number;
+    offsetLeft: number;
+    offsetTop: number;
+    tooltipRight: boolean;
   }
 
   let stage: SVGSVGElement;
-  let focusingNode: NodeData;
+  let focusingNode: Node;
 
-  const onMouseOverNode = (pointData: NodeData) => {
-    focusingNode = pointData;
+  const onMouseOverNode = (pointData: Node) => {
+    const offsetLeft = Math.round(pointData.x + stage.clientWidth / 2);
+    const offsetTop = Math.round(pointData.y + stage.clientHeight / 2);
+    focusingNode = {
+      ...pointData,
+      offsetLeft,
+      offsetTop,
+      tooltipRight: offsetLeft < stage.clientWidth / 2,
+    };
   };
 
   const onMouseOutOfNode = () => {
     focusingNode = null;
   };
 
-  const onClickNode = (pointData: NodeData) => {
+  const onClickNode = (pointData: Node) => {
     console.log(pointData);
   };
 
@@ -40,12 +52,20 @@
 
 <div class="relative flex-1">
   <svg bind:this={stage} class="w-full h-full" />
+
   {#if focusingNode}
     <div
-      class="absolute top-2 right-2 w-48 break-words bg-black bg-opacity-50 text-white p-2 rounded"
+      class="absolute"
+      style="top: {focusingNode.offsetTop}px; left: {focusingNode.offsetLeft}px;"
     >
-      <Typography as="subtitle5" bold>{focusingNode.name}</Typography>
-      <Typography as="subtitle5">{formatDate(focusingNode.date)}</Typography>
+      <div
+        class="absolute bottom-2 {focusingNode.tooltipRight
+          ? 'left-1'
+          : 'right-1'} w-48 break-words bg-black bg-opacity-50 text-white p-2 rounded"
+      >
+        <Typography as="subtitle5" bold>{focusingNode.name}</Typography>
+        <Typography as="subtitle5">{formatDate(focusingNode.date)}</Typography>
+      </div>
     </div>
   {/if}
 </div>
