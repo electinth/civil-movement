92f9614165061ec9dc99038891f030a13972b304
Th1nkK1D <Th1nkK1D@users.noreply.github.com>
2021-01-21 11:52:00 +0000

    deploy: e527b9698ac804dd19115e61d873cb94cf2aebc9

--- client/summary.6e485d7e.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as a,s as r,g as e,r as t,c as u,b as c,u as m,d as o,f as i,m as l,F as n}from"./client.2f9ee879.js";function f(s){let a,r;return{c(){a=e("div"),r=t("summary page")},l(s){a=u(s,"DIV",{});var e=c(a);r=m(e,"summary page"),e.forEach(o)},m(s,e){i(s,a,e),l(a,r)},p:n,i:n,o:n,d(s){s&&o(a)}}}export default class extends s{constructor(s){super(),a(this,s,null,f,r,{})}}
