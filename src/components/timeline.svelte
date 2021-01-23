f43063b3815667045f4496c64617445a3f48f5d6
thasarito <thasarit.pysr@gmail.com>
2021-01-23 22:16:45 +0700

    add overlay

--- src/components/timeline.svelte
+++ src/components/timeline.svelte
@@ -1,33 +1,42 @@
 <script lang="ts">
-import { createEventDispatcher } from "svelte";
+  import { createEventDispatcher } from 'svelte';
 
-  const dispatch = createEventDispatcher()
-  export const minmax: [number, number] = [0, 14]
-  const [min, max] = minmax
+  const dispatch = createEventDispatcher();
+  export const minmax: [number, number] = [0, 14];
+  const [min, max] = minmax;
 
-  $: range = [...Array(max - min).keys()]
+  $: range = [...Array(max - min).keys()];
 
   let slider, width: number, height: number;
 
-  const values = [4, 12]
+  const startend = [4, 12];
+  const [start, end] = startend;
 
+  $: unitWidth = width / range.length;
 </script>
 
-<div id="rangeSlider" class="w-full h-full flex bg-white" bind:clientWidth={width} bind:clientHeight={height} bind:this={slider}>
+<div
+  id="rangeSlider"
+  class="w-full h-full flex bg-white relative"
+  bind:clientWidth={width}
+  bind:clientHeight={height}
+  bind:this={slider}
+>
   {#each range as month}
-    <div class="section text-center border-r-2 my-3 border-gray" style="flex: 0 0 {width / range.length}px;">
+    <div
+      class="section text-center border-r-2 my-3 border-gray"
+      style="flex: 0 0 {unitWidth}px;"
+    >
       {month}
     </div>
   {/each}
-  {#each values as value, index}
-    <span
-      id="rangeHandle"
-     />
+  <div
+    id="overlay"
+    class="absolute bg-mint h-full opacity-25"
+    style="left: {unitWidth * start}px; width: {unitWidth *
+      (end - start)}px; mix-blend-mode: multiply;"
+  />
+  {#each startend as value, index}
+    <span id="rangeHandle" class="absolute" />
   {/each}
 </div>
-
-<style>
-  .section {
-
-  }
-</style>
