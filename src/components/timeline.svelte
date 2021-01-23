fe2e5b7ef59b9d1263ca4c4f07b0a445d6a2779c
thasarito <thasarit.pysr@gmail.com>
2021-01-23 22:24:51 +0700

    add handler

--- src/components/timeline.svelte
+++ src/components/timeline.svelte
@@ -5,14 +5,67 @@
   export const minmax: [number, number] = [0, 14];
   const [min, max] = minmax;
 
-  $: range = [...Array(max - min).keys()];
+  $: values = [...Array(max - min).keys()];
 
   let slider, width: number, height: number;
 
   const startend = [4, 12];
   const [start, end] = startend;
 
-  $: unitWidth = width / range.length;
+  $: stepSize = width / values.length;
+  $: step = values.length;
+
+  function sliderInteractStart(e: MouseEvent) {
+    console.log('--- slider interact start ---');
+    console.log(e);
+    e.preventDefault();
+    const handle = e.target as HTMLDivElement,
+      activeHandleIndex = +handle.id.slice(-1);
+
+    const currentValue = values[activeHandleIndex];
+    let previousValue = alignValueToStep(currentValue);
+    eventStart();
+  }
+
+  function sliderInteractEnd(e: MouseEvent) {
+    console.log('--- slider interact end ---');
+    console.log(e);
+
+    eventStop();
+  }
+
+  function sliderBlurHandle(e: FocusEvent) {
+    console.log('--- slider blur handle ---');
+    console.log(e);
+
+    eventChange();
+  }
+
+  $: alignValueToStep = function (val: number): number {
+    if (val <= min) {
+      return min;
+    } else if (val >= max) {
+      return max;
+    }
+    let remainder = (val - min) % step;
+    let aligned = val - remainder;
+    if (Math.abs(remainder) * 2 >= step) {
+      aligned += remainder > 0 ? step : -step;
+    }
+    return aligned;
+  };
+
+  function eventStart() {
+    dispatch('start', {});
+  }
+
+  function eventStop() {
+    dispatch('stop', {});
+  }
+
+  function eventChange() {
+    dispatch('change', {});
+  }
 </script>
 
 <div
@@ -22,21 +75,28 @@
   bind:clientHeight={height}
   bind:this={slider}
 >
-  {#each range as month}
+  {#each values as month}
     <div
       class="section text-center border-r-2 my-3 border-gray"
-      style="flex: 0 0 {unitWidth}px;"
+      style="flex: 0 0 {stepSize}px;"
     >
       {month}
     </div>
   {/each}
   <div
     id="overlay"
-    class="absolute bg-mint h-full opacity-25"
-    style="left: {unitWidth * start}px; width: {unitWidth *
+    class="absolute bg-mint h-full opacity-25 pointer-events-none"
+    style="left: {stepSize * start}px; width: {stepSize *
       (end - start)}px; mix-blend-mode: multiply;"
   />
   {#each startend as value, index}
-    <span id="rangeHandle" class="absolute" />
+    <div
+      id="handle-{index}"
+      class="absolute h-full bg-black opacity-25 cursor-pointer"
+      style="left: {stepSize * (value - 0.25)}px; width: {stepSize / 2}px;"
+      on:mousedown={sliderInteractStart}
+      on:mouseup={sliderInteractEnd}
+      on:blur={sliderBlurHandle}
+    />
   {/each}
 </div>
