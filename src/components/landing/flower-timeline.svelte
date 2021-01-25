83fd8ba6b921f0f6786ee19c1227054cfca6aaf1
thasarito <thasarit.pysr@gmail.com>
2021-01-25 22:07:13 +0700

    refactor Scale to flower component #8

--- src/components/landing/flower-timeline.svelte
+++ src/components/landing/flower-timeline.svelte
@@ -4,8 +4,11 @@
   import movements from '../../assets/data/event_all.csv';
   import Linechart from '../linechart.svelte';
   import formatMovementData from '../../utils/formatMovementToLinedata';
+  import { extent, max, scaleLinear, scaleTime } from 'd3';
 
   const linedata = formatMovementData(movements);
+  $: Y = scaleLinear().domain([0, max(linedata, (d) => d.y)]);
+  $: X = scaleTime().domain(extent(linedata, (d) => d.x));
 
   let values = [0, 5];
   const timelineRanges = linedata.map((d) => d.x);
@@ -16,7 +19,7 @@
     class="absolute w-full pointer-events-none"
     style="transform: translate(0, -100%);"
   >
-    <Linechart data={linedata} />
+    <Linechart data={linedata} {Y} {X} />
   </div>
   <div class="">
     <Timeline bind:values rangesItem={timelineRanges} />
--- src/components/linechart.svelte
+++ src/components/linechart.svelte
@@ -4,23 +4,20 @@
     extent,
     line,
     max,
+    ScaleLinear,
     scaleLinear,
+    ScaleTime,
     scaleTime,
   } from 'd3';
   import type { TData } from '../utils/formatMovementToLinedata';
 
   export let data: TData[] = [];
+  export let Y: ScaleLinear<number, number, never> = scaleLinear();
+  export let X: ScaleTime<number, number, never> = scaleTime();
 
-  let width = 1000,
+  export let width = 1000,
     height = 500,
     margin = { top: 0, right: 0, bottom: 0, left: 0 };
-
-  $: Y = scaleLinear()
-    .domain([0, max(data, (d) => d.y)])
-    .range([height - margin.bottom, margin.top]);
-  $: X = scaleTime()
-    .domain(extent(data, (d) => d.x))
-    .range([margin.left, width - margin.right]);
 
   $: pathDef = line<TData>()
     .x((d) => X(d.x))
@@ -28,7 +25,7 @@
     .curve(curveCatmullRom);
 </script>
 
-<div class="w-full h-full" bind:clientWidth={width} bind:clientHeight={height}>
+<div class="w-full h-full">
   <svg class="h-full w-full">
     <defs>
       <linearGradient id="whiteGradient" gradientTransform="rotate(90)">
