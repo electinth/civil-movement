4afdfbcdd3215e1b67def97eecf7e909753b1465
Th1nkK1D <witheep@gmail.com>
2021-02-15 14:52:41 +0700

    Fix sumaary list font

--- src/components/summary/topic-ordered-list.svelte
+++ src/components/summary/topic-ordered-list.svelte
@@ -1,5 +1,9 @@
-<ol
-  class="list-inside list-decimal flex flex-col justify-center items-center font-normal font-body"
->
-  <slot />
+<script>
+  import Typography from '../typography.svelte';
+</script>
+
+<ol class="list-inside list-decimal flex flex-col justify-center items-center">
+  <Typography as="body1">
+    <slot />
+  </Typography>
 </ol>
