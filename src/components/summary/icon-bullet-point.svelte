ca18c17935f6661a89b35f2fa998129abd1f84a1
Th1nkK1D <witheep@gmail.com>
2021-02-15 16:53:56 +0700

    Add summary missing icon and update typo

---
+++ src/components/summary/icon-bullet-point.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Typography from '../typography.svelte';
+
+  export let label: string;
+</script>
+
+<div class="flex-1 flex flex-row md:flex-col md:text-center md:justify-center">
+  <div class="flex flex-row md:mx-auto h-12 w-20">
+    <div class="m-auto">
+      <slot />
+    </div>
+  </div>
+  <Typography as="body1" class="my-auto md:my-0 md:mb-auto">{label}</Typography>
+</div>
--- src/components/summary/topic.svelte
+++ src/components/summary/topic.svelte
@@ -1,3 +1,3 @@
-<div class="text-white text-center max-w-2xl my-10">
+<div class="text-white text-center max-w-3xl my-10">
   <slot />
 </div>
